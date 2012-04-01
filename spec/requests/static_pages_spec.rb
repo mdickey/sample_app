3bb0160e65d52bbc8d6b1759b75b0d207b75d9e9
Michael DIckey <mdickey62@gmail.com>
2012-03-31 23:33:44 -0700

    things

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -34,12 +34,12 @@ describe "StaticPages" do
 
   describe "About page" do
 
- 	it "should have the h1 'About'" do
- 		visit '/static_pages/about'
- 		page.should have_selector('h1', :text => 'About Us')
- 	end
+ 	  it "should have the h1 'About'" do
+ 		   visit '/static_pages/about'
+ 		   page.should have_selector('h1', :text => 'About Us')
+ 	  end
 
- 	it "should have the title 'About'" do
+ 	  it "should have the title 'About'" do
       visit '/static_pages/about'
       page.should have_selector('title',
       				:text => "#{base_title} | About Us")
@@ -49,12 +49,12 @@ describe "StaticPages" do
 
   describe "Contact page" do
 
- 	it "should have the h1 'Contact'" do
- 		visit '/static_pages/contact'
- 		page.should have_selector('h1', :text => 'Contact')
- 	end
+ 	  it "should have the h1 'Contact'" do
+ 		   visit '/static_pages/contact'
+ 		   page.should have_selector('h1', :text => 'Contact')
+ 	  end
 
- 	it "should have the title 'Contact'" do
+ 	  it "should have the title 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('title',
       				:text => "#{base_title} | Contact")
